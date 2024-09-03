3ccbcda87be589b47fdbe8f341f838d8b4a5fd3c
Matías Angélico <mnangelico@gmail.com>
2024-09-03 00:20:22 -0300

    ContrasteTemporal, desviacionEstandar, media

--- descriptores.py
+++ descriptores.py
@@ -10,3 +10,21 @@ def fujii(tensor):
     x2 =np.abs(tensor[:,:,0:frames-1]+tensor[:,:,1:frames])
     x2[x2 == 0] = 1
     return np.sum(x1/x2,axis=2)
+
+def desviacionEstandar(tensor):
+    frames = tensor.shape[2]
+    return np.std(tensor[:,:,0:frames],axis=2)
+
+def contrasteTemporal(tensor):
+    frames = tensor.shape[2]
+    return np.std(tensor[:,:,0:frames],axis=2)/media(tensor)
+
+def media(tensor):
+    frames = tensor.shape[2]
+    return np.mean(tensor[:,:,0:frames],axis=2)
+
+def correlacionCruzada(tensor):
+    return np.apply_along_axis(correlacionCruzada1d,axis=2, arr = tensor)
+
+def correlacionCruzada1d(arr):
+    return np.correlate(arr, arr, mode='full')
--- speckleLaser.py
+++ speckleLaser.py
@@ -8,4 +8,10 @@ m = np.array(data['video_data']).transpose(1,2,0)
 a = np.array(ds.diferenciasPromediadas(m))
 b = np.array(ds.fujii(m))
 
-print(b)
+tensor = np.array([[[1,2,3,14],[42,5,6,7],[6,27,8,9]],[[110,11,12,13],[132,14,15,16],[172,17,18,19]],[[20,1,22,23],[23,24,25,26],[26,27,28,29]]])
+
+def correlacion_cruzada_1d(arr):
+    return np.correlate(arr, arr, mode='full')
+
+#print(tensor[:,:,0:4])
+print(np.array(ds.correlacionCruzada(tensor)))
