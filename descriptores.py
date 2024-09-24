4948b989c5cf218f61f859b2cb8c58238c57701d
Matías Angélico <mnangelico@gmail.com>
2024-09-24 15:19:28 -0300

    mas descriptores

--- descriptores.py
+++ descriptores.py
@@ -1,21 +1,32 @@
 import numpy as np
-from scipy.signal import welch
 from scipy.stats import entropy
 
+frames = 0
+
+def setearFrames (nroFrames):
+    global frames
+    frames = nroFrames
+
+def diferenciasPesadas(tensor):
+    alto = tensor.shape[0]
+    ancho = tensor.shape[1]
+    peso = 5
+    difPesadas = np.zeros((alto,ancho,frames-peso))
+    for c in range(frames - peso):
+        difPesadas[:, :, c] = np.abs(tensor[:, :, c] * (peso - 1) - np.sum(tensor[:, :, c+1:c+peso+1], axis=2))
+    return difPesadas
+
+
 def diferenciasPromediadas(tensor):
-    frames = tensor.shape[2]
     return np.sum(np.abs(tensor[:,:,0:frames-1]-tensor[:,:,1:frames]),axis=2)/(frames-1)
 
 def fujii(tensor):
-    frames = tensor.shape[2]
     x1 =np.abs(tensor[:,:,0:frames-1]-tensor[:,:,1:frames])
     x2 =np.abs(tensor[:,:,0:frames-1]+tensor[:,:,1:frames])
     x2[x2 == 0] = 1
     return np.sum(x1/x2,axis=2)
 
 def entropiaShannon(tensor):
-
-    frames = tensor.shape[2]
     
     # Inicializar una matriz para almacenar la entropía de Shannon para cada píxel
     entropy_matrix = np.zeros(tensor.shape[:2])
@@ -36,19 +47,69 @@ def entropiaShannon(tensor):
     return entropy_matrix
 
 def desviacionEstandar(tensor):
-    frames = tensor.shape[2]
     return np.std(tensor[:,:,0:frames],axis=2)
 
 def contrasteTemporal(tensor):
-    frames = tensor.shape[2]
     return np.std(tensor[:,:,0:frames],axis=2)/media(tensor)
 
 def media(tensor):
-    frames = tensor.shape[2]
-    return np.mean(tensor[:,:,0:frames],axis=2)
+    return np.mean(tensor,axis=2, keepdims=True)
 
-def correlacionCruzada(tensor):
-    return np.apply_along_axis(correlacionCruzada1d,axis=2, arr = tensor)
+def autocorrelacionFFT(tensor): #tarda un 25% menos que haciendo los dos ciclos for
+    from numpy.fft import fft, ifft 
+    tensor_fft = fft(tensor, n=2*frames-1, axis=2)
+    result = np.real(ifft(tensor_fft * np.conj(tensor_fft),axis=2))
+    return np.concatenate((result[:,:,frames:],result[:,:,:frames]),axis=2)
 
-def correlacionCruzada1d(arr):
-    return np.correlate(arr, arr, mode='full')
+def autoCorrelacion(tensor):
+    auto_corr = np.zeros((tensor.shape[0],tensor.shape[1],frames*2-1))
+    for i in range(tensor.shape[0]):
+        for j in range(tensor.shape[1]):
+            auto_corr[i, j, :] = np.correlate(tensor[i, j, :], tensor[i, j, :], mode='full')
+        
+    return auto_corr
+
+def fuzzy(tensor,threshold):
+    diff = np.abs(np.diff(tensor, axis=2))
+    return np.where(diff > threshold, 1, 0)
+
+def frecuenciaMedia(tensor):
+    from scipy.signal import welch
+    f, Pxx = welch(tensor)
+    return np.sum(Pxx * f, axis=2) /np.sum(Pxx, axis=2)
+
+
+def entropiaShannon1(tensor):
+    from scipy.signal import welch
+    _, Pxx = welch(tensor, axis=2)
+    return -np.sum ((Pxx /np.sum(Pxx, axis=2, keepdims =True))* np.log2(Pxx/np.sum(Pxx, axis=2, keepdims=True )+ 1e-12), axis=2)
+
+def frecuenciaCorte(tensor):
+    from scipy.signal import welch
+    #f , Pxx = welch(tensor - media(tensor))
+    #f_welch, x_PSD = welch(x - np.mean(x))
+    '''
+    Pxx_half = np.expand_dims(Pxx[1], axis=0) / 2
+    return 0 if np.all(Pxx[1] <= 0) else (
+        f[-1] if len(np.where(Pxx) - Pxx_half <= 0)[0] == 0 
+        else f[np.where(Pxx - Pxx_half <= 0)[0][0] + 1]
+    )
+    '''
+    f_welch, x_PSD = welch(tensor - np.mean(tensor))
+    
+    # Verificar si el segundo valor de x_PSD es <= 0
+    if np.all(x_PSD[1] <= 0,axis=2):
+        return 0
+    else:
+        # Calcular la diferencia del PSD con respecto a la mitad del segundo valor
+        D_PS = x_PSD - x_PSD[1] / 2
+        # Encontrar la primera frecuencia donde D_PS es menor o igual a 0
+        ll = np.where(D_PS[1:] <= 0)[0]
+        
+        if len(ll) == 0:
+            # Si no se encuentra ninguna frecuencia, usar la última frecuencia disponible
+            return f_welch[-1]
+        else:
+            # Devolver la frecuencia correspondiente
+            return f_welch[ll[0] + 1]
+    #return -np.sum ((Pxx /np.sum(Pxx, axis=2, keepdims =True))* np.log2(Pxx/np.sum(Pxx, axis=2, keepdims=True )+ 1e-12), axis=2)
--- speckleLaser.py
+++ speckleLaser.py
@@ -2,26 +2,33 @@ import numpy as np
 import scipy.io as sio
 import descriptores as ds
 
-data = sio.loadmat('matrizmoneda10.mat')
-m = np.array(data['video_data']).transpose(1,2,0)
+#data = sio.loadmat('matrizmoneda10.mat')
+#m = np.array(data['video_data']).transpose(1,2,0)
+#ds.setearFrames(m.shape[2])
 
-a = np.array(ds.diferenciasPromediadas(m))
-b = np.array(ds.fujii(m))
-c = np.array(ds.entropiaShannon(m))
-
-<<<<<<< HEAD
 tensor = np.array([[[1,2,3,14],[42,5,6,7],[6,27,8,9]],[[110,11,12,13],[132,14,15,16],[172,17,18,19]],[[20,1,22,23],[23,24,25,26],[26,27,28,29]]])
+ds.setearFrames(4)
 
-def correlacion_cruzada_1d(arr):
-    return np.correlate(arr, arr, mode='full')
+#a = np.array(ds.diferenciasPromediadas(m))
+#b = np.array(ds.fujii(m))
+#c = np.array(ds.entropiaShannon(m))
+#d = np.array(ds.diferenciasPesadas(m))
+#e = np.array(ds.autocorrelacionFFT(m))
+#f = np.array(ds.autoCorrelacion(m))
+#g = np.array(ds.frecuenciaMedia(m))
+#h = np.array(ds.entropiaShannon1(m))
 
-#print(tensor[:,:,0:4])
-print(np.array(ds.correlacionCruzada(tensor)))
-=======
+#print(timeit.timeit("ds.autoCorrelacion(m)",globals=globals(),number=1))
+#print(timeit.timeit("ds.autocorrelacionFFT(m)",globals=globals(),number=1))
 
-print(b)
+np.set_printoptions(suppress=True,precision=2)
+#print(tensor[0,0,:])
+#print (ds.diferenciasPesadas(tensor))
+#print (np.sum(np.stack([tensor[:,:,:ki-1] for ki in range(4)],axis=2 ),axis=2))
+#print(e.shape)
+#print(e)
+#print(f)
+#print (np.allclose(e,f))
 
-
-print("ENTROPIA DE SHANNON")
-print(c)
->>>>>>> 8c215df5dd93d320fdcc03ceca99e757d9356d31
+#print (ds.fuzzy(tensor,10))
+print(ds.frecuenciaCorte(tensor))
