845ff0900c519ab7b6115b32b4d12f56bd5ad231
ignaciosuarezquilis <ignaciogustavosuarez1234@gmail.com>
2024-11-04 19:02:09 -0300

    aparecio auth0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import UploadVideo from './subirVideos.jsx';
 import DefaultValuesManager from './DefaultValuesManager';
+import BarraSuperior from './BarraSuperior.jsx';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
+
+    const {loginWithRedirect} = useAuth0()
+
     return (
         <div>
             <h1>Subir Video AVI</h1>
+            <button onClick={() => loginWithRedirect()}>Login</button>
+            <BarraSuperior/>
             <DefaultValuesManager />
             <UploadVideo />
         </div>
