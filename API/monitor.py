3cf496cc7354d8a396faa0bd1494524e9dc736bf
Lautaro Nahuel Bruses <lautarobruses@gmail.com>
2025-07-17 18:33:41 -0300

    delete datos de prueba

--- API/monitor.py
+++
@@ -1,8 +0,0 @@
-import psutil
-import time
-
-while True:
-    cpu = psutil.cpu_percent(interval=1)
-    ram = psutil.virtual_memory().percent
-    print(f"CPU: {cpu}%, RAM: {ram}%")
-    time.sleep(2)
